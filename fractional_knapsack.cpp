a3c5dc217a6be7b205e6377a878fc3307c46829b
Mohamad Khaled <mohamad.khaledmorsy@gmail.com>
2023-08-11 23:24:24 +0300

    fractional_knapsack enhanced

--- fractional_knapsack.cpp
+++ fractional_knapsack.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
-double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
+/*double get_optimal_value_v1(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
   int weight = 0;
   double best = 0;
@@ -17,7 +18,6 @@ double get_optimal_value(int capacity, vector<int> weights, vector<int> values) 
       index = i;
     }
   }
-
   if (capacity == 0 || weight == 0)
   {
     return 0;
@@ -27,7 +27,26 @@ double get_optimal_value(int capacity, vector<int> weights, vector<int> values) 
   value = values[index] * (amount / weights[index]);
   values.erase(values.begin() + index);
   weights.erase(weights.begin() + index);
-  return value + get_optimal_value(capacity, weights, values);;
+  return value + get_optimal_value_v1(capacity, weights, values);
+}*/
+
+double get_optimal_value_v2(int capacity, vector<int> weights, vector<int> values) {
+  double value = 0.0;
+  vector<std::pair<double, int>> v(weights.size());
+
+  for (int i = 0; i < weights.size(); i++)
+  {
+    v[i] = {double(values[i]) / double(weights[i]), i};
+  }
+  sort(v.rbegin(), v.rend());
+
+  for (int i = 0; i < weights.size() && capacity > 0; i++) {
+    int index = v[i].second;
+    double amount = std::min(capacity, weights[index]);
+    capacity -= amount;
+    value += amount * v[i].first;
+  }
+  return value;
 }
 
 int main() {
@@ -40,7 +59,7 @@ int main() {
     std::cin >> values[i] >> weights[i];
   }
 
-  double optimal_value = get_optimal_value(capacity, weights, values);
+  double optimal_value = get_optimal_value_v2(capacity, weights, values);
 
   std::cout.precision(10);
   std::cout << optimal_value << std::endl;
